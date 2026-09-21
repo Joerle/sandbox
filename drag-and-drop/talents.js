a4c462ecbbf9a190a77a2f56222badc964a98a4d
joerle <joerle.josh@gmail.com>
2026-09-20 23:19:25 -0700

    refactor[drag] Update data structure to separate selected/available

--- drag-and-drop/talents.js
+++ drag-and-drop/talents.js
@@ -1,38 +1,41 @@
 const TALENTS = {
-    "utility": [
-        { "name": "Basic Aerokinesis", "level": "zero", "default": "selected", "class": "air" },
-        { "name": "Basic Geokinesis", "level": "zero", "default": "available", "class": "earth" },
-        { "name": "Glass Cannon", "level": "zero", "default": "selected", "class": "earth" },
-
-        { "name": "Elemental Whispers", "level": "one", "default": "available", "class": "universal" },
-        { "name": "Elemental Grip", "level": "three", "default": "available", "class": "universal" },
-
-        { "name": "Voice of the Wind", "level": "one", "default": "selected", "class": "air" },
-        { "name": "Air's Leap", "level": "one", "default": "available", "class": "air" },
-        { "name": "Air Cushion", "level": "one", "default": "selected", "class": "air" },
-        { "name": "Wings of Air", "level": "three", "default": "selected", "class": "air" },
-        { "name": "Greater Voice of the Wind", "level": "two", "default": "available", "class": "air" },
-        { "name": "Windsight", "level": "three", "default": "available", "class": "air" },
-        { "name": "Greater Windsight", "level": "five", "default": "available", "class": "air" },
-        { "name": "Aerial Evasion", "level": "three", "default": "available", "class": "air" },
-        { "name": "Body of Air", "level": "four", "default": "available", "class": "air" },
-        
-        { "name": "Suffocate", "level": "six", "default": "available", "class": "air" },
-
-        { "name": "Earth Walk", "level": "one", "default": "available", "class": "earth" },
-        { "name": "Earth Climb", "level": "two", "default": "available", "class": "earth" },
-        { "name": "Earthmeld", "level": "four", "default": "available", "class": "earth" },
-        { "name": "Earth Glide", "level": "five", "default": "available", "class": "earth" },
-        { "name": "Earth Tongue", "level": "eight", "default": "available", "class": "earth" },
-        { "name": "Kinetic Cover", "level": "one", "default": "available", "class": "earth" },
-        { "name": "Shift Earth", "level": "four", "default": "available", "class": "earth" },
-        { "name": "Greater Shift Earth", "level": "four", "default": "available", "class": "earth" },
-        { "name": "Stone Sculptor", "level": "five", "default": "available", "class": "earth" }
-    ],
-    "infusion": [
-        { "name": "Extended Range", "level": "one", "default": "selected", "class": "universal" },
-        { "name": "Burning Infusion", "level": "one", "default": "selected", "class": "air" },
-        { "name": "Penetrating Infusion", "level": "two", "default": "selected", "class": "air" },
-        { "name": "Flurry of Blasts", "level": "three", "default": "selected", "class": "universal" }
-    ]
+    "utility": {
+        "selected": [
+            { "name": "Basic Aerokinesis", "level": "zero", "class": "air" },
+            { "name": "Glass Cannon", "level": "zero", "class": "earth" },
+            { "name": "Voice of the Wind", "level": "one", "class": "air" },
+            { "name": "Air Cushion", "level": "one", "class": "air" },
+            { "name": "Wings of Air", "level": "three", "class": "air" }
+        ],
+        "available": [
+            { "name": "Basic Geokinesis", "level": "zero", "class": "earth" },
+            { "name": "Elemental Whispers", "level": "one", "class": "universal" },
+            { "name": "Elemental Grip", "level": "three", "class": "universal" },
+            { "name": "Air's Leap", "level": "one", "class": "air" },
+            { "name": "Greater Voice of the Wind", "level": "two", "class": "air" },
+            { "name": "Windsight", "level": "three", "class": "air" },
+            { "name": "Greater Windsight", "level": "five", "class": "air" },
+            { "name": "Aerial Evasion", "level": "three", "class": "air" },
+            { "name": "Body of Air", "level": "four", "class": "air" },
+            { "name": "Suffocate", "level": "six", "class": "air" },
+            { "name": "Earth Walk", "level": "one", "class": "earth" },
+            { "name": "Earth Climb", "level": "two", "class": "earth" },
+            { "name": "Earthmeld", "level": "four", "class": "earth" },
+            { "name": "Earth Glide", "level": "five", "class": "earth" },
+            { "name": "Earth Tongue", "level": "eight", "class": "earth" },
+            { "name": "Kinetic Cover", "level": "one", "class": "earth" },
+            { "name": "Shift Earth", "level": "four", "class": "earth" },
+            { "name": "Greater Shift Earth", "level": "four", "class": "earth" },
+            { "name": "Stone Sculptor", "level": "five", "class": "earth" }
+        ]
+    },
+    "infusion": {
+        "selected": [
+            { "name": "Extended Range", "level": "one", "class": "universal" },
+            { "name": "Burning Infusion", "level": "one", "class": "air" },
+            { "name": "Penetrating Infusion", "level": "two", "class": "air" },
+            { "name": "Flurry of Blasts", "level": "three", "class": "universal" }
+        ],
+        "available": []
+    }
 };
